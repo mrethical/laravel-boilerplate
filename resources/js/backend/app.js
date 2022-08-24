d22e43a6c86fa0faa7a7700a06ced1733ee90883
Jefferson Magboo <jeffersonmagboo21@gmail.com>
2022-08-24 23:37:47 +0800

    Add prettier

--- resources/js/backend/app.js
+++ resources/js/backend/app.js
@@ -1,10 +1,10 @@
 import 'alpinejs'
 
-window.$ = window.jQuery = require('jquery');
-window.Swal = require('sweetalert2');
+window.$ = window.jQuery = require('jquery')
+window.Swal = require('sweetalert2')
 
 // CoreUI
-require('@coreui/coreui');
+require('@coreui/coreui')
 
 // Boilerplate
-require('../plugins');
+require('../plugins')
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,5 @@
-window._ = require('lodash');
-window.Swal = require('sweetalert2');
+window._ = require('lodash')
+window.Swal = require('sweetalert2')
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -8,10 +8,10 @@ window.Swal = require('sweetalert2');
  */
 
 try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
+  window.Popper = require('popper.js').default
+  window.$ = window.jQuery = require('jquery')
 
-    require('bootstrap');
+  require('bootstrap')
 } catch (e) {}
 
 /**
@@ -20,9 +20,9 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = require('axios')
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
